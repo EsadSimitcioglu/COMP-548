1964608b65b372435e2c2b10338a0defac7541c4
EsadSimitcioglu <esad.simitcioglu@ozu.edu.tr>
2023-04-23 02:11:53 +0300

    Finish the part 1 and started the part 2 again

--- HW1/FindCellLocations.py
+++ HW1/FindCellLocations.py
@@ -1,31 +1,87 @@
+import copy
+
 import cv2
+import imutils as imutils
 import numpy as np
 
 from metric import pixel_level
 
-ground_truth = np.loadtxt('data/im1_gold_mask.txt')
+
+def otus_method(rgb_image):
+    # Calculate histogram
+    histogram = cv2.calcHist([rgb_image], [0], None, [256], [0, 256])
+
+    # Normalize the histogram
+    histogram_norm = histogram.ravel() / histogram.sum()
+
+    # Compute the cumulative distribution function (CDF)
+    cdf = histogram_norm.cumsum()
+
+    # Initialize variables
+    best_threshold = 0
+    best_intra_class_variance = 0
+
+    # Iterate over all possible threshold values
+    for t in range(256):
+        # Background and foreground class probabilities
+        prob_background = cdf[t]
+        prob_foreground = 1 - prob_background
+
+        # Background and foreground class means
+        epsilon = 1e-8  # small epsilon value to avoid division by zero
+        mean_background = (histogram_norm[:t + 1] * np.arange(t + 1)).sum() / max(prob_background, epsilon)
+        mean_foreground = (histogram_norm[t + 1:] * np.arange(t + 1, 256)).sum() / max(prob_foreground, epsilon)
+
+        # Compute intra-class variance
+        intra_class_variance = prob_background * prob_foreground * ((mean_background - mean_foreground) ** 2)
+
+        # Update the best threshold if necessary
+        if intra_class_variance > best_intra_class_variance:
+            best_intra_class_variance = intra_class_variance
+            best_threshold = t
+
+    return best_threshold
+
+
 rgb_image = cv2.imread('data/im1.jpg', cv2.IMREAD_GRAYSCALE)
+mask_image = cv2.imread('output/im1.jpg', cv2.IMREAD_GRAYSCALE)
+kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
+best_threshold = otus_method(rgb_image)
 
-pixel_gray_value_list = list()
+binary_image = cv2.threshold(rgb_image, best_threshold, 255, cv2.THRESH_BINARY)[1]
+opened_binary_image = cv2.morphologyEx(binary_image, cv2.MORPH_OPEN, kernel, iterations=1)
+cells = cv2.bitwise_not(opened_binary_image, mask=mask_image)
+eroded_cells = cv2.erode(cells, kernel, iterations=1)
 
-for row in range(768):
-    for column in range(1024):
+dist = cv2.distanceTransform(eroded_cells, cv2.DIST_L2, 3)
+cv2.normalize(dist, dist, 0, 255, cv2.NORM_MINMAX)
+cv2.imwrite('distTransformed.jpg', dist)
 
-        if ground_truth[row][column] == 1 and rgb_image[row][column] > 180:
-            ground_truth[row][column] = 0
-            pixel_gray_value_list.append(rgb_image[row][column])
+binary = dist.astype('uint8')
+
+# Find contours in the binary image
+contours = cv2.findContours(binary, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+# Iterate through contours and calculate x and y distances
+for contour in contours:
+    # Calculate the centroid of the contour
+    M = cv2.moments(contour)
+    cX = int(M["m10"] / M["m00"])
+    cY = int(M["m01"] / M["m00"])
+
+    # Calculate the x and y distances from the centroid to the image center
+    center_x = rgb_image.shape[1] // 2
+    center_y = rgb_image.shape[0] // 2
+    x_distance = cX - center_x
+    y_distance = cY - center_y
+
+    print("Contour Centroid: ({}, {})".format(cX, cY))
+    print("X Distance from Center: {}".format(x_distance))
+    print("Y Distance from Center: {}".format(y_distance))
+    print("---------------------")
 
 
-# Calculate dictionary of pixel gray values and their frequency
-pixel_gray_value_dict = dict()
-for pixel_gray_value in pixel_gray_value_list:
-    if pixel_gray_value in pixel_gray_value_dict:
-        pixel_gray_value_dict[pixel_gray_value] += 1
-    else:
-        pixel_gray_value_dict[pixel_gray_value] = 1
-
-print(pixel_gray_value_dict)
-
-cv2.imshow('Original Image', ground_truth)
+cv2.imshow('Eroded Cells', eroded_cells)
+cv2.imshow('Dist', dist)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
--- HW1/ObtainForegroundMax.py
+++ HW1/ObtainForegroundMax.py
@@ -3,10 +3,8 @@ import numpy as np
 
 from metric import pixel_level
 
-
 img_list = ['im1.jpg', 'im2.jpg', 'im3.jpg']
 ground_truth_list = ['im1_gold_mask.txt', 'im2_gold_mask.txt', 'im3_gold_mask.txt']
-
 
 for img_index in range(len(img_list)):
 
@@ -37,8 +35,8 @@ for img_index in range(len(img_list)):
 
         # Background and foreground class means
         epsilon = 1e-8  # small epsilon value to avoid division by zero
-        mean_background = (histogram_norm[:t+1] * np.arange(t+1)).sum() / max(prob_background, epsilon)
-        mean_foreground = (histogram_norm[t+1:] * np.arange(t+1, 256)).sum() / max(prob_foreground, epsilon)
+        mean_background = (histogram_norm[:t + 1] * np.arange(t + 1)).sum() / max(prob_background, epsilon)
+        mean_foreground = (histogram_norm[t + 1:] * np.arange(t + 1, 256)).sum() / max(prob_foreground, epsilon)
 
         # Compute intra-class variance
         intra_class_variance = prob_background * prob_foreground * ((mean_background - mean_foreground) ** 2)
@@ -47,19 +45,25 @@ for img_index in range(len(img_list)):
         if intra_class_variance > best_intra_class_variance:
             best_intra_class_variance = intra_class_variance
             best_threshold = t
-    print(best_threshold)
 
-    # Apply the threshold
-    binary_image = cv2.threshold(test_image, best_threshold, 1, cv2.THRESH_BINARY)[1]
+    edges = cv2.Canny(test_image, 100, 200)  # You can adjust the thresholds as needed
 
-    # Define the erosion kernel (structuring element)
-    kernel = np.ones((4, 4), dtype=np.uint8)  # 3x3 square kernel
+    binary_image = cv2.threshold(test_image, best_threshold, 255, cv2.THRESH_BINARY)[1]
 
-    # Perform binary erosion
-    dilated_image = cv2.dilate(binary_image, kernel, iterations=13)
-    eroded_image = cv2.erode(dilated_image, kernel, iterations=14)
+    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
 
-    precision, recall, f_score = pixel_level(ground_truth, eroded_image)
+    # dilated_image = cv2.dilate(binary_image, kernel, iterations=20)
+    closed_image = cv2.morphologyEx(binary_image, cv2.MORPH_CLOSE, kernel, iterations=20)
+
+    cv2.imwrite('output/' + img_list[img_index], closed_image)
+
+    # cv2.imshow('Dilated Image', closed_image)
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
+
+    closed_image[closed_image == 255] = 1
+
+    precision, recall, f_score = pixel_level(ground_truth, closed_image)
 
     print("Precision:" + str(precision))
     print("Recall: " + str(recall))
