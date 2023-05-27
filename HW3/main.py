a1a7a48e87c26f95691063a3aa0202d1d683fc6d
EsadSimitcioglu <esad.simitcioglu@ozu.edu.tr>
2023-05-28 00:38:47 +0300

    Refactor the weighted class

--- HW3/main.py
+++ HW3/main.py
@@ -88,17 +88,16 @@ def train_model(model, criterion, optimizer, scheduler, num_epochs):
 
 
 def make_weights_for_balanced_classes(images, nclasses):
-    count = [0] * nclasses
-    for item in images:
-        count[item[1]] += 1
-    weight_per_class = [0.] * nclasses
-    N = float(sum(count))
-    for i in range(nclasses):
-        weight_per_class[i] = N/float(count[i])
-    weight = [0] * len(images)
-    for idx, val in enumerate(images):
-        weight[idx] = weight_per_class[val[1]]
-    return weight
+    count = defaultdict(int)
+
+    for _, label in images:
+        count[label] += 1
+
+    weight_per_class = [len(images) / count[i] for i in range(nclasses)]
+
+    weights = [weight_per_class[label] for _, label in images]
+
+    return weights
 
 
 data_dir = 'dataset'
